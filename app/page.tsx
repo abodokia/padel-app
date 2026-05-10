c565930cf0936431ea3df40d85a0d922891b9042
abodokia <aniibodokia@gmail.com>
2026-05-11 00:54:46 +0400

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-"use client";
-
-import { HomeView } from "@/components/home-view";
-
-/** Today’s court grid; unauthenticated visitors are sent to `/login` (or `/login?mode=signup`). */
-export default function Home() {
-  return <HomeView />;
+export default function SimplePage() {
+  return (
+    <div style={{ background: "red", color: "white", height: "100vh", padding: "50px", textAlign: "center" }}>
+      <h1>წითელი ეკრანი!</h1>
+      <p>თუ ამას ხედავ, ესე იგი საიტი ცოცხალია და პრობლემა AppProviders-ში იყო.</p>
+    </div>
+  );
 }
